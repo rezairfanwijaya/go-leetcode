9a68ee525d5053a7cd69063cc809012a4ab0992b
Reza Irfan Wijaya <rezaaagit@gmail.com>
2024-02-04 13:45:56 +0700

    add: complete parentheses

--- valid-parentheses/valid_parentheses.go
+++ valid-parentheses/valid_parentheses.go
@@ -1,56 +1,30 @@
 package validparentheses
 
 func IsValid(s string) bool {
-	lengthS := len(s)
-
-	// * akan false ketika length 0, 1 atau ganjil
-	if lengthS == 0 || lengthS == 1 || lengthS%2 != 0 {
+	if len(s)%2 != 0 || len(s) == 0 {
 		return false
 	}
 
-	// * memisahkan open dan close
-	parenthesesOpen := make(map[string]int)
-	parenthesesClose := make(map[string]int)
-
-	// * memasukan open dan close kedalam map
-	for index, v := range s {
-		if string(v) == "(" || string(v) == "{" || string(v) == "[" {
-			parenthesesOpen[string(v)] = index
-		} else {
-			parenthesesClose[string(v)] = index
-		}
+	stack := []rune{}
+	opener := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
 	}
 
-	// * cek open dan close dengan kondisi
-	// * index open < index close
-	for _, v := range s {
-		if string(v) == "(" {
-			isValid := checkIndex("(", ")", parenthesesOpen, parenthesesClose)
-			if !isValid {
-				return false
-			}
-		} else if string(v) == "{" {
-			isValid := checkIndex("{", "}", parenthesesOpen, parenthesesClose)
-			if !isValid {
-				return false
-			}
-		} else if string(v) == "[" {
-			isValid := checkIndex("{", "}", parenthesesOpen, parenthesesClose)
-			if !isValid {
-				return false
-			}
+	for _, letter := range s {
+		if closer, ok := opener[letter]; ok {
+			stack = append(stack, closer)
+			continue
 		}
+
+		idx := len(stack) - 1
+		if idx < 0 || stack[idx] != letter {
+			return false
+		}
+
+		stack = stack[:idx]
 	}
 
-	return true
-}
-
-// * index open harus lebih kecil dari index close
-func checkIndex(
-	openString, closeString string,
-	openParentheses, closeParentheses map[string]int) bool {
-
-	indexOpen := openParentheses[openString]
-	indexClose := closeParentheses[closeString]
-	return indexOpen < indexClose
+	return len(stack) == 0
 }
